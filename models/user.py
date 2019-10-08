f442a537622309c5daf5df34668443c2cb8c37c6
Mahapatra, Sarthak <Sarthak_Mahapatra@Dell.com>
2019-10-09 02:57:37 +0530

    Added google auth

--- models/user.py
+++ models/user.py
@@ -1,15 +1,33 @@
-class User():
-    
-    def __init__(self, name,token,role):
+from flask_login import UserMixin
+import pymongo
+
+from db import db
+
+
+class User(UserMixin):
+    def __init__(self, id_, name, email, role):
+        self.id = id_
         self.name = name
-        self.token = token
+        self.email = email
         self.role = role
 
     def asdict(self):
         return {
-            'name':self.name,
-            'token':self.token,
-            'role':self.role
+            'id_': self.id,
+            'name': self.name,
+            'email': self.email,
+            'role': self.role
         }
 
+    @classmethod
+    def get(cls, user_id):
+        user = db.users.find_one({"id_": user_id})
+        if user:
+            _, id_, name, email, role = user
+            return cls(id_, name, email, role)
+        return None
 
+    @classmethod
+    def create(cls, id_, name, email, role):
+        user = cls(id_, name, email, role)
+        db.users.insert_one(user.asdict())
