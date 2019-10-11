12d991a80358c6f8167115eae4689da58ec7362f
Rohan Panda <rohan.panda1@gmail.com>
2019-10-11 19:47:16 +0530

    Add session api

--- app.py
+++ app.py
@@ -2,12 +2,17 @@
 from flask import Flask
 from flask import request, jsonify
 import json
-from flask_pymongo import PyMongo
+# from flask_pymongo import PyMongo
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mydb"
-mongo = PyMongo(app)
-db = mongo.db
+client = MongoClient()
+db = client.mydb
+
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/mydb"
+# mongo = PyMongo(app)
+# db = mongo.db
 
 @app.route('/', methods=['GET'])
 def home():
@@ -21,6 +26,35 @@ def test():
     for s in users:
         output.append({'name' : s['name'], 'email' : s['email']})
     return jsonify({'result' : output})
+
+@app.route('/createSession', methods=['POST'])
+def createSession():
+    params = request.get_json()
+
+    doc_id = db.sessions.insert({
+        "type": params['type'],
+        "userId": params['userId'],
+        "username": params['username'],
+        "requestedAt": params['requestedAt'],
+        "status": params['status'],
+    })
+    return str(doc_id)
+
+@app.route('/updateSession', methods=['POST'])
+def updateSession():
+    params = request.get_json()
+
+    sessionId = params['sessionId']
+
+    response = db.sessions.update({ "_id": ObjectId(sessionId) }, {'$set': {
+        "status": params['status'],
+        "mentorname": params['mentorname'],
+        "mentorId": params['mentorId']
+    }})
+    if (response['updatedExisting']):
+        return 'updated'
+    else:
+        return 'error'
 
 
 if __name__ == '__main__':
