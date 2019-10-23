b6c862f7f84f2f478a7cfb01e55e328c2c86d05a
princepritam <prince.pritam@weinvest.net>
2019-10-23 21:52:35 +0530

    added CRUD for session

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import code, os, json, datetime
 from db import db
 # Third party libraries
 from flask import Flask, redirect, url_for, request, jsonify
-from bson.json_util import dumps
+from bson import ObjectId, errors
 from pymodm.connection import connect
 from pymongo.errors import DuplicateKeyError
 # Connect to MongoDB and call the connection "onx".
@@ -20,9 +20,9 @@ app = Flask(__name__)
 @app.route("/user/create", methods=['POST'])
 def create_user():
     params = request.get_json()
-    # code.interact(local=dict(globals(), **locals()))
     try:
-        user = User(params['email'], params['name'], params['mobileNo'], params['role'], datetime.datetime.now())
+        user = User(email=params['email'], name=params['name'], mobileNo=params['mobileNo'],
+                    role=params['role'], created_at=datetime.datetime.now(), updated_at=datetime.datetime.now())
         user.save(force_insert=True)
     except (DuplicateKeyError, ValidationError) as e:
         return jsonify({'Error': str(e), 'error_status': True})
@@ -32,39 +32,84 @@ def create_user():
 def get_all_users():
     users_list = []
     for user in User.objects.all():
-        users_list.append({'name':user.name, 'email':user.email, 'mobileNo':user.mobileNo, 'role':user.role, 'created_at':user.created_at, 'updated_at':user.updated_at})
+        users_list.append({'id': str(user._id), 'name':user.name, 'email':user.email, 
+                                'mobileNo':user.mobileNo, 'role':user.role, 'created_at':user.created_at, 'updated_at':user.updated_at})
     return jsonify(users_list)
 
-@app.route("/user/update/<string:email>", methods=['PATCH'])
-def update_user(email):
-    update_params = {}
-    valid_params = ['name', 'mobileNo', 'role', 'email']
-    for key, value in request.get_json().items():
-        if key in valid_params:
-            update_params[key] = value
+@app.route("/user/<string:id>", methods=['GET'])
+def show_user(id):
     try:
-        User.objects.get({'email':email})
+        id = ObjectId(id)
+        user = User.objects.get({'_id': id})
+        return jsonify([{'id': str(user._id), 'name':user.name, 'email':user.email, 
+                        'mobileNo':user.mobileNo, 'role':user.role, 'created_at':user.created_at, 'updated_at':user.updated_at}])
+    except (User.DoesNotExist, errors.InvalidId) as e :
+        message = 'User does not exists.' if str(e) == '' else str(e)
+        return jsonify({'Error': message, 'error_status': True})
+
+
+@app.route("/user/update/<string:id>", methods=['PATCH'])
+def update_user(id):
+    try:
+        id = ObjectId(id)
+        update_params = {}
+        valid_params = ['name', 'mobileNo', 'role', 'email']
+        for key, value in request.get_json().items():
+            if key in valid_params:
+                update_params[key] = value
+        User.objects.get({'_id':id})
         user = User.from_document(update_params)
         user.full_clean(exclude=None)
         update_params['updated_at'] = datetime.datetime.now()
-        User.objects.raw({'email':email}).update({'$set': update_params})
-    except (User.DoesNotExist, ValidationError) as e:
+        User.objects.raw({'_id': id}).update({'$set': update_params})
+    except (User.DoesNotExist, ValidationError, errors.InvalidId) as e:
+        message = 'User does not exists.' if str(e) == '' else str(e)
         # code.interact(local=dict(globals(), **locals()))
-        return jsonify({'Error': str(e), 'error_status': True})
+        return jsonify({'Error': message, 'error_status': True})
     return jsonify({'Message': 'User updated successfully.', 'error_status': False})
 
-@app.route("/user/delete/<string:email>", methods=['DELETE'])
-def delete_user(email):
+@app.route("/user/delete/<string:id>", methods=['DELETE'])
+def delete_user(id):
     try:
-        User.objects.get({'email':email}).delete()
-    except User.DoesNotExist:
-        return jsonify({'Error': 'User does not exists.', 'error_status': True})
+        id = ObjectId(id)
+        User.objects.get({'_id':id}).delete()
+    except (User.DoesNotExist, errors.InvalidId) as e:
+        message = 'User does not exists.' if str(e) == '' else str(e)
+        return jsonify({'Error': message, 'error_status': True})
     return jsonify({'Message': 'User deleted from database.', 'error_status': False})
 
 #CRUD for Session
 @app.route("/session/create", methods=['POST'])
 def create_session():
-    params = request.get_json()
+    create_params = request.get_json()
+    try:
+        mentor = User.objects.get({'_id': ObjectId(create_params['mentor_id'])})
+        student = User.objects.get({'_id': ObjectId(create_params['student_id'])})
+        session = Session(type_=create_params['type'], mentor=mentor, student=student, created_at=datetime.datetime.now(), updated_at=datetime.datetime.now())
+        session.save(force_insert=True)
+    except(User.DoesNotExist, ValidationError) as e:
+        return jsonify({'Error': str(e), 'error_status': True})
+    return jsonify({'Message': 'Successfully created session.','error_status': False})
+
+@app.route("/sessions", methods=['GET', 'POST'])
+def get_all_Sessions():
+    sessions_list = []
+    for session in Session.objects.all():
+        sessions_list.append({'id': str(session._id), 'type': session.type_, 'mentor': session.mentor.email, 'student': session.student.email, 'start_time':
+                    session.start_time, 'status': session.status, 'duration': session.duration, 'end_time': session.end_time,
+                    'feedback': session.feedback, 'created_at': session.created_at, 'updated_at': session.updated_at})
+    return jsonify(sessions_list)
+
+@app.route("/update/session/<string:id>", methods=['POST'])
+def update_session(id):
+    try:
+        id = ObjectId(id)
+        code.interact(local=dict(globals(), **locals()))
+        session = Session.objects.get({'_id': id})
+    except(Session.DoesNotExist) as e :
+        return jsonify({'Error': str(e), 'error_status': True})
+    return jsonify({'Message': 'Successfully updated session.','error_status': False})
+
 
 
 
--- app/models/session.py
+++ app/models/session.py
@@ -1,13 +1,28 @@
 from pymongo.write_concern import WriteConcern
 from pymodm import MongoModel, fields
 from app.models import User
+from pymodm.errors import ValidationError
+
+def validate_mentor(user):
+    if not user.role == 'mentor':
+        raise ValidationError('Given user is not having role as mentor')
+
+def validate_student(user):
+    if not user.role == 'student':
+        raise ValidationError('Given user is not having role as student')
 
 class Session(MongoModel):
-    mentor = fields.ReferenceField(User, mongo_name='mentor')
-    student = fields.ReferenceField(User, mongo_name='student')
-    start_time = fields.DateTimeField(mongo_name='start_time')
+    type_ = fields.CharField(required=True, choices=['single','multiple'], mongo_name='type')
+    mentor = fields.ReferenceField(User, mongo_name='mentor', validators=[validate_mentor])
+    student = fields.ReferenceField(User, mongo_name='student', validators=[validate_student])
+    members = fields.ListField(mongo_name='members')
+    status = fields.CharField(choices=('active', 'inactive'), mongo_name='status')
+    start_time = fields.DateTimeField(required=False, mongo_name='start_time')
     duration = fields.TimestampField(mongo_name='duration')
     end_time = fields.DateTimeField(mongo_name='end_time')
+    feedback = fields.CharField(max_length=1000, mongo_name='feed_back')
+    created_at = fields.DateTimeField(required=True, mongo_name='created_at')
+    updated_at = fields.DateTimeField(mongo_name='updated_at')
     class Meta:
         write_concern = WriteConcern(j=True)
         ignore_unknown_fields = True
