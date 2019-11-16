1e80a2dcbea4b791384b528db0c9ea6b1e4d5575
princepritam <princepritam1996@gmail.com>
2019-11-16 23:52:06 +0530

    fixed duration calculation

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ deploy = "mongodb://testuser:qwerty123@ds241258.mlab.com:41258/heroku_mjkv6v40"
 # deploy="mongodb://heroku_mjkv6v40:osce9dakl9glgd4750cuovm8h1@ds241258.mlab.com:41258/heroku_mjkv6v40"
 local = "mongodb://localhost:27017/onx"
 
-connect(local, alias="onx-app", retryWrites=False)
+connect(deploy, alias="onx-app", retryWrites=False)
 
 
 app = Flask(__name__)
@@ -321,5 +321,5 @@ def handle_my_custom_event(json):
 	emit('test', 'received json on channel: ' + str(json))
 
 if __name__ == '__main__':
-	# socketio.run(app)
-	app.run(port=3000, debug=True, host='localhost')
+	socketio.run(app)
+	# app.run(port=3000, debug=True, host='localhost')
