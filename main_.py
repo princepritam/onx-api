47430cda7c72f13a5cb21a1b39873f15d4924e09
princepritam <princepritam1996@gmail.com>
2019-12-19 11:43:53 +0530

    fixed corporate code issue

---
+++ main_.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, Flask, request, jsonify
+from pymodm.connection import connect
+from app import app, socketio
+
+deploy = "mongodb://testuser:qwerty123@ds241258.mlab.com:41258/heroku_mjkv6v40"
+local = "mongodb://localhost:27017/onx"
+
+connect(deploy, alias="onx-app", retryWrites=False)
+
+app.config['SECRET_KEY'] = 'unxunxunx'
+
+# Health check
+@app.route("/ping", methods=['GET'])
+def home():
+    return 'Hello! Your app is up and running.'
+
+if __name__ == '__main__':
+    # socketio.run(app)
+    app.run(port=3000, debug=True, host='localhost')
